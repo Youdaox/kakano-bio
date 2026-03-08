8680d8232a95e753f12a3e933a34b7d8785bf889
Jason <jasonlimnzz@gmail.com>
2026-03-08 19:50:13 +1300

    starting hero

--- components/sections/homepage/hero.tsx
+++ components/sections/homepage/hero.tsx
@@ -1,8 +1,29 @@
+"use client"
+
+
+import Image from 'next/image';
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 py-20 text-center">
-      <h1 className="text-4xl font-bold text-gray-800">Welcome to Kakano Biosciences</h1>
-      <p className="text-lg text-gray-600">Innovating the future of biotechnology</p>
+    <div className="relative w-full h-96 flex items-center justify-center">
+      <Image
+        src="/pe.avif"
+        alt="Hero background"
+        fill
+        className="object-cover -z-10"
+        priority
+      />
+      <motion.div
+      initial={{ opacity: 0, y: 20}}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      >
+        <div className="flex flex-col items-center justify-center gap-4 text-center text-white">
+          <h1 className="text-5xl font-bold text-white">Welcome to Kakano Biosciences</h1>
+          <p className="text-2xl text-white">Global Innovation Specialists</p>
+        </div>
+      </motion.div>
     </div>
   );
 };
