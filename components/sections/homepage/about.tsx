27fc1022e4008aaaaaa55d77df54c4f64ca2d2a6
Youdaox <youdao005@gmail.com>
2026-03-16 01:16:23 +1300

    modifed about

--- components/sections/homepage/about.tsx
+++ components/sections/homepage/about.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <section className="w-full">
+    <section className="w-full mx-auto px-32">
       <div className="flex min-h-128 w-full py-12">
         {/* Left side - Text */}
         <div className="w-1/2 flex items-center bg-transparent px-10 lg:px-14">
@@ -12,16 +12,22 @@ export default function About() {
                 Aoteroa&apos;s partner for innovative, culturally grounded molecular diagnostics — 
                 combining global scientific leadership with deep local relationships.
             </p>
+            <div className="mt-6 flex gap-4">
+              <button className="btn-primary mt-6">Contact us</button>
+              <button className="btn-primary bg-transparent outline-gray-800 outline-1 text-gray-800 mt-6">Learn More</button>
+            </div>
           </div>
         </div>
 
         {/* Right side - Image */}
-        <div className="w-1/2 relative min-h-128">
+        <div className="relative w-3xl aspect-9/6 rounded-sm overflow-hidden">
           <Image
             src="/nz1.avif"
             alt="About section image"
             fill
             className="object-cover"
+            sizes="(max-width: 1024px) 100vw, 50vw"
+            priority
           />
         </div>
       </div>
